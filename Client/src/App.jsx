bebb67df150beaa450ed1b3d9b5b3d1cbd0a3147
Shuvam-2000 <shuvamsaha2000@gmail.com>
2025-06-19 19:19:53 +0530

    feat: admin panel components added

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,9 +1,15 @@
-import { Route, Routes, BrowserRouter, useLocation } from "react-router-dom";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Navbar from "./components/common/Navbar";
 import Footer from "./components/common/Footer";
 import Home from "./components/blog/Home";
 import Blogs from "./components/blog/Blogs";
 import Login from "./components/common/Login";
+import Layout from "./components/admin/Layout";
+import Dashboard from "./components/admin/Dashboard";
+import AddBlog from "./components/admin/AddBlog";
+import ListBlog from "./components/admin/ListBlog";
+import Comments from "./components/admin/Comments";
+import { useLocation } from "react-router-dom";
 import "./index.css";
 
 function LayoutWrapper({ children }) {
@@ -19,16 +25,47 @@ function LayoutWrapper({ children }) {
   );
 }
 
+function AppRoutes() {
+  const location = useLocation();
+
+  // Check if the route starts with "/admin"
+  const isAdminRoute = location.pathname.startsWith("/admin");
+
+  return (
+    <Routes>
+      {!isAdminRoute ? (
+        // Routes WITH LayoutWrapper 
+        <Route
+          path="*"
+          element={
+            <LayoutWrapper>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/blog/:id" element={<Blogs />} />
+                <Route path="/login" element={<Login />} />
+              </Routes>
+            </LayoutWrapper>
+          }
+        />
+      ) : (
+        // Admin Routes 
+        <>
+          <Route path="/admin" element={<Layout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="addblog" element={<AddBlog />} />
+            <Route path="listblog" element={<ListBlog />} />
+            <Route path="comments" element={<Comments />} />
+          </Route>
+        </>
+      )}
+    </Routes>
+  );
+}
+
 function App() {
   return (
     <BrowserRouter>
-      <LayoutWrapper>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/blog/:id" element={<Blogs />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </LayoutWrapper>
+      <AppRoutes />
     </BrowserRouter>
   );
 }
